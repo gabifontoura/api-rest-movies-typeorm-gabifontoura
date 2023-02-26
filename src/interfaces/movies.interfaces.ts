89c55f3ff14b7f94cc11a00762499262ba1e2249
Gabriela <gabrielagarciafontoura@gmail.com>
2023-02-26 20:01:36 -0300

    feat: DER

--- src/interfaces/movies.interfaces.ts
+++ src/interfaces/movies.interfaces.ts
@@ -1,5 +1,6 @@
-import { DeepPartial } from "typeorm"
+import { DeepPartial, Repository } from "typeorm"
 import { z } from "zod"
+import Movie from "../entities"
 import { movieSchema, returnMovieSchema, returnAllMoviesSchema } from "../schemas/movies.schemas"
 
 export type iMovie = z.infer<typeof movieSchema>
@@ -9,3 +10,5 @@ export type iMovieResult = z.infer<typeof returnMovieSchema>
 export type iAllMoviesResult = z.infer<typeof returnAllMoviesSchema>
 
 export type iUpdateMovie = DeepPartial<iMovie>
+
+export type  iMovieRepo = Repository<Movie>
--- src/services/listAllMovies.services.ts
+++ src/services/listAllMovies.services.ts
@@ -5,11 +5,19 @@ import { iAllMoviesResult } from "../interfaces/movies.interfaces"
 import { returnAllMoviesSchema } from "../schemas/movies.schemas"
 
 
-export const listAllMoviesService = async (): Promise<iAllMoviesResult> => {
+export const listAllMoviesService = async (payload:any): Promise<iAllMoviesResult> => {
 
     const movieRepository: Repository<Movie> = AppDataSource.getRepository(Movie)
+    const page: number = parseInt(payload.page) || 1
+    const perPage: number = parseInt(payload.perPage) || 5
 
-    const findMovies: Array<Movie> = await movieRepository.find()
+    const findMovies: Array<Movie> = await movieRepository.find({
+        take: perPage, 
+        skip: perPage * (page-1), 
+        order: {
+            name: 'ASC'
+        }
+    })
 
     const users = returnAllMoviesSchema.parse(findMovies)
 
